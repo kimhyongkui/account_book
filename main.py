155f0a5208399f0e45bff6dab7b1196eaa5dcdc4
Kim Hyeong Kyu <hyongkui@nate.com>
2023-03-29 23:53:00 +0900

    put, post 라우터 추가

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from routers.post import join
+from routers.put import edit_account
 import uvicorn
 
 app = FastAPI(title="account_book")
+
+app.include_router(join.router, prefix="/account-book")
+app.include_router(edit_account.router, prefix="/account-book")
 
 if __name__ == "__main__":
     uvicorn.run(
