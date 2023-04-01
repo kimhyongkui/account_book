6052be67e9f5bb2204af7052bfb044b48a3cd2d0
Kim Hyeong Kyu <hyongkui@nate.com>
2023-04-01 21:48:57 +0900

    _init_추가, 예외폴더 추가

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
-from routers.post import join
-from routers.put import edit_account
+from routers.create import join, write_account_book, login
+from routers.read import get_user_info, get_account_book
+from routers.put import edit_user_info, edit_account_book, restore_data
+from routers.delete import delete_user_info, delete_account_book
 import uvicorn
 
 app = FastAPI(title="account_book")
 
 app.include_router(join.router, prefix="/account-book")
-app.include_router(edit_account.router, prefix="/account-book")
+app.include_router(login.router, prefix="/account-book")
+app.include_router(write_account_book.router, prefix="/account-book")
+
+app.include_router(get_user_info.router, prefix="/account-book")
+app.include_router(get_account_book.router, prefix="/account-book")
+
+app.include_router(edit_user_info.router, prefix="/account-book")
+app.include_router(edit_account_book.router, prefix="/account-book")
+app.include_router(restore_data.router, prefix="/account-book")
+
+app.include_router(delete_user_info.router, prefix="/account-book")
+app.include_router(delete_account_book.router, prefix="/account-book")
 
 if __name__ == "__main__":
     uvicorn.run(
