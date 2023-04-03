d5f40610f71111b06db9217e487f5ce51508f174
Kim Hyeong Kyu <hyongkui@nate.com>
2023-04-03 23:39:35 +0900

    put->patch로 변경

--- routers/patch/edit_account_book.py
+++ routers/patch/edit_account_book.py
@@ -5,7 +5,7 @@ from app.auth import has_permission
 router = APIRouter()
 
 
-@router.put('/account-book/{user_id}/update', tags=["update"])
+@router.patch('/account-book/{user_id}/update', tags=["update"])
 def edit_account_book_info(no, amount, date, memo, user_id: str = Depends(has_permission)):
     result = edit_account_book(no, user_id, amount, date, memo)
     return result
--- routers/patch/edit_user_info.py
+++ routers/patch/edit_user_info.py
@@ -5,7 +5,7 @@ from db.update.user_info import edit_user
 router = APIRouter()
 
 
-@router.put('/account/{user_id}/update', tags=["update"])
+@router.patch('/account/{user_id}/update', tags=["update"])
 def edit_user_info(email, pwd, user_id: str = Depends(has_permission)):
     result = edit_user(user_id, email, pwd)
     return result
