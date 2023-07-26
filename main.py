c64ab6cc4cb784bac27eb978ca74e1acf2684fd7
Kim Hyeong Kyu <hyongkui@nate.com>
2023-07-26 22:47:27 +0900

    배포환경/로컬환경 구분

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from routers.read import get_user_info, get_account_book
 from routers.patch import edit_user_info, edit_account_book, restore_data
 from routers.delete import delete_user_info, delete_account_book
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="account_book")
-
 
 app.include_router(join.router, prefix="/account-book")
 app.include_router(login.router, prefix="/account-book")
@@ -23,20 +26,18 @@ app.include_router(restore_data.router, prefix="/account-book")
 app.include_router(delete_user_info.router, prefix="/account-book")
 app.include_router(delete_account_book.router, prefix="/account-book")
 
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "main:app",
-#         host="0.0.0.0",
-#         reload=True,
-#         port=8080
-#     )
 
-# development only
+def main():
+    environment = os.getenv("ENVIRONMENT")
+    if environment == "local":
+        host = "localhost"
+        port = 8050
+    else:
+        host = os.getenv("SERVER_HOST")
+        port = int(os.getenv("SERVER_PORT"))
+
+    uvicorn.run("main:app", host=host, port=port, reload=True)
+
+
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="localhost",
-        reload=True,
-        port=8000
-    )
-    
+    main()
